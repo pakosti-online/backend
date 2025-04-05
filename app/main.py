76a228217f6fe29e3752e2712a14e27595c1ad9d
Максим Захаренков <m.zaharenkov@mail.ru>
2025-04-06 01:08:49 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from app.views import api_router
 from fastapi import FastAPI
 from app.db import init_db
@@ -6,6 +7,14 @@ from app.db import init_db
 def create_application():
     app = FastAPI(title="Pakosti Online", docs_url="/swagger")
     app.include_router(api_router)
+
+    # we do real PAKOSTI here
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     init_db(app)
 
