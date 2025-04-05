0a8d577989afc40f9f1974bfe38a7d81c8c28014
Максим Захаренков <m.zaharenkov@mail.ru>
2025-04-05 22:49:31 +0300

    Обращение к модели за категорией транзакции

--- app/controllers/categories.py
+++ app/controllers/categories.py
@@ -1,10 +1,21 @@
-from app.models.transaction import TransactionCategoryModel
 from app.schemas.transaction import TransactionCategoryOutDto
+from app.models.transaction import TransactionCategoryModel
+from os import environ
+import httpx
+
+PREDICTOR_CATEGORIZER_URL = environ.get(
+    "EXTERNAL_PREDICTOR_CATEGORIZER_URL", "http://localhost:3000/"
+)
 
 
-# TODO
 async def get_category(product_name: str):
-    return await TransactionCategoryModel.get_or_none(name="Зарплата")
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            PREDICTOR_CATEGORIZER_URL, json={"word": product_name}
+        )
+
+        category_name = response.text
+        return await TransactionCategoryModel.get_or_none(name=category_name)
 
 
 async def get_all_categories() -> list[TransactionCategoryOutDto]:
